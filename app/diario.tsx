8dbe55d24f587b99a6bf67f791e9a0cd20b8236c
Mario <1833050@alu.murciaeduca.es>
2025-05-03 12:46:02 +0200

    enlaces aux

--- app/diario.tsx
+++ app/diario.tsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, Image, TouchableOpacity, Modal, TextInput, FlatList, ActivityIndicator, Alert, RootTagContext } from 'react-native';
 import { Colors } from '@/themes/Colors';
 import { useNavigation } from '@react-navigation/native';
+import { Linking } from 'react-native';
+
 
 const allLabels = [
     'Agua', 'Fuego', 'Planta', 'Eléctrico', 'Hielo', 'Tierra', 'Bicho', 'Fantasma',
@@ -61,6 +63,58 @@ export default function Diario() {
         Array(3).fill(null).map(() => Array(3).fill(null))
     );
     const regions = ['Kanto','Johto','Hoenn','Sinnoh','Teselia','Kalos','Alola','Galar','Paldea','Hisui'];
+
+    const abrirInfo = (etiqueta: string) => {
+        // Normaliza para URL
+        const nombre = etiqueta.toLowerCase();
+      
+        // Puedes personalizar cada ruta
+        const urls: Record<string, string> = {
+          // Tipos
+          fuego: 'https://bulbapedia.bulbagarden.net/wiki/Fire_(type)',
+          agua: 'https://bulbapedia.bulbagarden.net/wiki/Water_(type)',
+          planta: 'https://bulbapedia.bulbagarden.net/wiki/Grass_(type)',
+          volador: 'https://bulbapedia.bulbagarden.net/wiki/Flying_(type)',
+          normal: 'https://bulbapedia.bulbagarden.net/wiki/Normal_(type)',
+          lucha: 'https://bulbapedia.bulbagarden.net/wiki/Fighting_(type)',
+          veneno: 'https://bulbapedia.bulbagarden.net/wiki/Poison_(type)',
+          electrico: 'https://bulbapedia.bulbagarden.net/wiki/Electric_(type)',
+          tierra: 'https://bulbapedia.bulbagarden.net/wiki/Ground_(type)',
+          psiquico:'https://bulbapedia.bulbagarden.net/wiki/Psychic_(type)',
+          roca:'https://bulbapedia.bulbagarden.net/wiki/Rock_(type)',
+          hielo:'https://bulbapedia.bulbagarden.net/wiki/Ice_(type)',
+          bicho:'https://bulbapedia.bulbagarden.net/wiki/Bug_(type)',
+          dragon:'https://bulbapedia.bulbagarden.net/wiki/Dragon_(type)',
+          fantasma:'https://bulbapedia.bulbagarden.net/wiki/Ghost_(type)',
+          siniestro:'https://bulbapedia.bulbagarden.net/wiki/Dark_(type)',
+          acero:'https://bulbapedia.bulbagarden.net/wiki/Steel_(type)',
+          hada:'https://bulbapedia.bulbagarden.net/wiki/Fairy_(type)',
+
+          // Regiones
+          kanto: 'https://bulbapedia.bulbagarden.net/wiki/List_of_Pokémon_by_Kanto_Pokédex_number',
+          johto: 'https://bulbapedia.bulbagarden.net/wiki/List_of_Pokémon_by_Johto_Pokédex_number',
+          hoen: 'https://bulbapedia.bulbagarden.net/wiki/List_of_Pokémon_by_Hoenn_Pokédex_number_in_Generation_VI',
+          sinnoh: 'https://bulbapedia.bulbagarden.net/wiki/List_of_Pokémon_by_Sinnoh_Pokédex_number',
+          teselia: 'https://bulbapedia.bulbagarden.net/wiki/List_of_Pokémon_by_Unova_Pokédex_number_in_Pokémon_Black_2_and_White_2',
+          kalos: 'https://bulbapedia.bulbagarden.net/wiki/List_of_Pokémon_by_Kalos_Pokédex_number',
+          alola: 'https://bulbapedia.bulbagarden.net/wiki/List_of_Pokémon_by_Alola_Pokédex_number_in_Pokémon_Ultra_Sun_and_Ultra_Moon',
+          galar: 'https://bulbapedia.bulbagarden.net/wiki/List_of_Pokémon_by_Galar_Pokédex_number',
+          paldea: 'https://bulbapedia.bulbagarden.net/wiki/List_of_Pokémon_by_Paldea_Pokédex_number',
+          hisui: 'https://bulbapedia.bulbagarden.net/wiki/List_of_Pokémon_by_Hisui_Pokédex_number',
+
+          //especiales
+          mega: 'https://bulbapedia.bulbagarden.net/wiki/Mega_Evolution',
+          'g-max': 'https://bulbapedia.bulbagarden.net/wiki/Gigantamax'
+        };
+      
+        const url = urls[nombre];
+        if (url) {
+          Linking.openURL(url);
+        } else {
+          console.warn('No hay URL para:', etiqueta);
+        }
+      };
+      
 
     useEffect(() => {
         const fetchAllPokemon = async () => {
@@ -248,7 +302,7 @@ export default function Diario() {
       
       
 
-    return (
+      return (
         <View style={styles.container}>
             <View style={styles.header}>
                 <Text style={styles.title}>Puzzle diario</Text>
@@ -256,31 +310,38 @@ export default function Diario() {
                     <Image source={require('../assets/images/tfg/help.png')} style={styles.infoIcon} />
                 </TouchableOpacity>
             </View>
-
+    
             <View style={styles.scoreContainer}>
                 <Text style={styles.scoreText}>Puntuación: 898</Text>
                 <Image source={require('../assets/images/tfg/masterball.png')} style={styles.scoreIcon} />
             </View>
-
+    
             <View style={styles.gridContainer}>
+                {/* Fila superior con las etiquetas */}
                 <View style={styles.row}>
                     <View style={styles.corner} />
                     {topLabels.map((label, idx) => (
-                        <View key={idx} style={styles.labelCell}>
-                            <Text style={styles.typeLabel}>{label}</Text>
-                        </View>
+                        <TouchableOpacity key={idx} style={[styles.labelCell,{marginRight:21,marginBottom:5}]} onPress={() => abrirInfo(label)}>
+                            <Text style={[styles.typeLabel,{marginLeft:25}]}>{label}</Text> 
+                        </TouchableOpacity>
                     ))}
                 </View>
-
+    
+                {/* Fila del tablero con las etiquetas a la izquierda */}
                 {Array.from({ length: 3 }).map((_, rowIdx) => (
                     <View key={rowIdx} style={styles.row}>
-                        <Text style={styles.typeLabel}>{leftLabels[rowIdx]}</Text>
+                        {/* Etiquetas de la izquierda */}
+                        <TouchableOpacity style={styles.labelCell} onPress={() => abrirInfo(leftLabels[rowIdx])}>
+                            <Text style={styles.typeLabel}>{leftLabels[rowIdx]}</Text>
+                        </TouchableOpacity>
+    
+                        {/* Celdas del tablero */}
                         {Array.from({ length: 3 }).map((_, colIdx) => {
                             const index = rowIdx * 3 + colIdx;
                             return (
                                 <TouchableOpacity key={colIdx} style={styles.cell} onPress={() => handleCellPress(index)}>
                                     {sprites[index] && (
-                                        <Image source={{ uri: sprites[index] }} style={{ width: 80, height: 80 }} />
+                                        <Image source={{ uri: sprites[index] }} style={{ width: 85, height: 85 }} />
                                     )}
                                 </TouchableOpacity>
                             );
@@ -288,34 +349,43 @@ export default function Diario() {
                     </View>
                 ))}
             </View>
-
+    
             <TouchableOpacity style={styles.surrenderButton} onPress={() => navigation.goBack()}>
                 <Text style={styles.surrenderText}>Rendirse</Text>
             </TouchableOpacity>
-
+    
             {/* Modal de búsqueda */}
             <Modal visible={modalVisible} animationType="slide">
                 <View style={{ flex: 1, backgroundColor: Colors.Fondo, padding: 20 }}>
-                    <Text style={{ color: Colors.blanco, fontSize: 20, fontFamily: 'Pixel',marginTop:10 }}>Buscar Pokémon</Text>
+                    <Text style={{ color: Colors.blanco, fontSize: 20, fontFamily: 'Pixel', marginTop: 10 }}>
+                        Buscar Pokémon
+                    </Text>
                     <TextInput
-                        style={{ backgroundColor: Colors.blanco, marginVertical: 35, borderRadius: 10, paddingHorizontal: 10,fontFamily:'Pixel',height:50,fontSize:18}}
+                        style={{
+                            backgroundColor: Colors.blanco,
+                            marginVertical: 35,
+                            borderRadius: 10,
+                            paddingHorizontal: 10,
+                            fontFamily: 'Pixel',
+                            height: 50,
+                            fontSize: 18,
+                        }}
                         placeholder="Nombre del Pokémon"
                         onChangeText={(text) => {
                             setQuery(text);
-                            const filtered = allPokemonNames.filter(name =>
+                            const filtered = allPokemonNames.filter((name) =>
                                 name.toLowerCase().includes(text.toLowerCase())
-                            ).slice(0, 10); // solo los primeros 10
-                        
+                            ).slice(0, 20); // cantidad de resultados al buscar
                             setResults(filtered);
-                        
+    
                             filtered.forEach(async (name) => {
                                 if (!pokemonSprites[name]) {
                                     try {
                                         const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
                                         const data = await res.json();
-                                        setPokemonSprites(prev => ({
+                                        setPokemonSprites((prev) => ({
                                             ...prev,
-                                            [name]: data.sprites.front_default
+                                            [name]: data.sprites.front_default,
                                         }));
                                     } catch (e) {
                                         console.error(`Error cargando sprite de ${name}`, e);
@@ -325,29 +395,31 @@ export default function Diario() {
                         }}
                         value={query}
                     />
-                    <TouchableOpacity 
+                    <TouchableOpacity
                         style={{
-                            width:150,
+                            width: 150,
                             backgroundColor: Colors.Botones_menu,
                             padding: 10,
                             borderRadius: 10,
                             alignItems: 'center',
-                            marginLeft:115,
-                            marginBottom:40
+                            marginLeft: 115,
+                            marginBottom: 40,
                         }}
                         onPress={() => setModalVisible(false)}
+                    >
+                        <Text
+                            style={{
+                                color: Colors.blanco,
+                                fontSize: 18,
+                                fontFamily: 'Pixel',
+                            }}
                         >
-                        <Text style={{
-                            color: Colors.blanco,
-                            fontSize: 18,
-                            fontFamily: 'Pixel',
-                        }}>
                             Volver
                         </Text>
                     </TouchableOpacity>
-
+    
                     {loading && <ActivityIndicator size="large" color={Colors.Botones_menu} />}
-
+    
                     <FlatList
                         data={results}
                         keyExtractor={(item) => item}
@@ -367,16 +439,15 @@ export default function Diario() {
                                 style={[styles.row, { alignItems: 'center', padding: 10 }]}
                             >
                                 {pokemonSprites[item] && (
-                                    <Image source={{ uri: pokemonSprites[item] }} style={{ width: 80, height: 80}} />
+                                    <Image source={{ uri: pokemonSprites[item] }} style={{ width: 80, height: 80 }} />
                                 )}
-                                <Text style={styles.typeLabel2}>{item}</Text>
+                                <Text style={styles.typeLabel2}>{String(item)}</Text> 
                             </TouchableOpacity>
                         )}
                     />
-
                 </View>
             </Modal>
-
+    
             {/* Modal de error */}
             <Modal
                 visible={errorModal.visible}
@@ -384,9 +455,33 @@ export default function Diario() {
                 animationType="fade"
                 onRequestClose={() => setErrorModal({ visible: false, message: '' })}
             >
-                <View style={{flex: 1,backgroundColor: 'rgba(0, 0, 0, 0.5)',justifyContent: 'center',alignItems: 'center',}}>
-                    <View style={{backgroundColor: Colors.Fondo,padding: 24,borderRadius: 20,borderColor: Colors.Tablero,borderWidth: 2,maxWidth: '80%'}}>
-                        <Text style={{fontFamily: 'Pixel',fontSize: 16,color: Colors.blanco,textAlign: 'center',marginBottom: 16}}>
+                <View
+                    style={{
+                        flex: 1,
+                        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                    }}
+                >
+                    <View
+                        style={{
+                            backgroundColor: Colors.Fondo,
+                            padding: 24,
+                            borderRadius: 20,
+                            borderColor: Colors.Tablero,
+                            borderWidth: 2,
+                            maxWidth: '80%',
+                        }}
+                    >
+                        <Text
+                            style={{
+                                fontFamily: 'Pixel',
+                                fontSize: 16,
+                                color: Colors.blanco,
+                                textAlign: 'center',
+                                marginBottom: 16,
+                            }}
+                        >
                             {errorModal.message}
                         </Text>
                         <TouchableOpacity
@@ -396,19 +491,22 @@ export default function Diario() {
                                 paddingVertical: 8,
                                 paddingHorizontal: 20,
                                 borderRadius: 10,
-                                alignSelf: 'center'
+                                alignSelf: 'center',
                             }}
                         >
-                            <Text style={{
-                                fontFamily: 'Pixel',
-                                fontSize: 14,
-                                color: Colors.blanco
-                            }}>Aceptar</Text>
+                            <Text
+                                style={{
+                                    fontFamily: 'Pixel',
+                                    fontSize: 14,
+                                    color: Colors.blanco,
+                                }}
+                            >
+                                Aceptar
+                            </Text>
                         </TouchableOpacity>
                     </View>
                 </View>
             </Modal>
-            {/*Modal de victoria*/}
             {/* Modal de victoria */}
             <Modal
                 animationType="fade"
@@ -472,7 +570,7 @@ const ficha_random = () => {
     const regionesBase = Object.keys(regionesPosibles);
     const regionesProbabilidad = [
       ...regionesBase,
-      'Mega', 'G-max', 'Mega', 'Mega', 'G-max', 'G-max', 'Mega' // duplicados solo para aumentar la probabilidad
+      'Mega', 'G-max', 'Mega', 'Mega', 'G-max', 'G-max', 'Mega','Mega', 'G-max', 'Mega', 'Mega', 'G-max', 'G-max', 'Mega' // duplicados solo para aumentar la probabilidad
     ];
     const tipos: string[] = Object.keys(tiposCombinables).map(t =>
       t.charAt(0).toUpperCase() + t.slice(1)
@@ -701,7 +799,7 @@ const styles = StyleSheet.create({
         height: 40,
         justifyContent: 'center',
         alignItems: 'center',
-        margin: 8,
-        marginRight: 10
+        marginRight:10,
+        marginLeft:-3        
     }
 });
